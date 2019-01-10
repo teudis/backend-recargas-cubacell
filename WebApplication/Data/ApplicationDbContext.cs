fe10e6b26bb5eff08198694e59ff8b783ebae7c9
Teudis Naranjo Ortiz <teudis2008@gmail.com>
2019-01-10 09:04:50 -0500

    Actualizacion de las relaciones mediante Fluent API
    Signed-off-by: Teudis Naranjo Ortiz <teudis2008@gmail.com>

--- WebApplication/Data/ApplicationDbContext.cs
+++ WebApplication/Data/ApplicationDbContext.cs
@@ -5,6 +5,10 @@ namespace SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Data
 {
     public class ApplicationDbContext : SmartSolucionesCuba.SAPRESSC.Core.Persistence.Context.AbstractTraceableIdentityDbContext<User>
     {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
         public DbSet<CellularBalanceTuneUpProfile> CellularBalanceTuneUpProfiles { get; set; }
         public DbSet<CellularBalanceTuneUpRecord> CellularBalanceTuneUpRecords { get; set; }
         public DbSet<CellularBalanceTuneUpRequest> CellularBalanceTuneUpRequests { get; set; }
@@ -16,8 +20,33 @@ namespace SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-            
-            builder.Entity<Account>().ToTable("Accounts");
+
+            new Persistence.Configurations.AccountEntityTypeConfigurator().Configure(builder, Database.GetDbConnection().Database);
+
+            foreach (var entity in builder.Model.GetEntityTypes())
+            {
+                entity.Relational().TableName = entity.Relational().TableName.ToLower();
+
+                foreach (var property in entity.GetProperties())
+                {
+                    property.Relational().ColumnName = property.Name.ToLower();
+                }
+
+                foreach (var key in entity.GetKeys())
+                {
+                    key.Relational().Name = key.Relational().Name.ToLower();
+                }
+
+                foreach (var key in entity.GetForeignKeys())
+                {
+                    key.Relational().Name = key.Relational().Name.ToLower();
+                }
+
+                foreach (var index in entity.GetIndexes())
+                {
+                    index.Relational().Name = index.Relational().Name.ToLower();
+                }
+            }
         }
 
         public override int SaveChanges()
