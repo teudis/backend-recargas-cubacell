158224f7ee3a73869bdd5d8d24e2dc5781b4669e
Teudis Naranjo Ortiz <teudis2008@gmail.com>
2019-01-10 09:21:57 -0500

    Repositorio para User
    Signed-off-by: Teudis Naranjo Ortiz <teudis2008@gmail.com>

---
+++ WebApplication/Data/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using SmartSolucionesCuba.SAPRESSC.Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Data.Persistence.Repositories
+{
+    public class UserRepository : AbstractEntityRepository<ApplicationDbContext, Entities.User, string>
+    {
+        public UserRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
