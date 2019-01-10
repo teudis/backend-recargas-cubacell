e24e883c3f326b79aef56c1e891461d8b07a863a
Teudis Naranjo Ortiz <teudis2008@gmail.com>
2019-01-10 09:10:00 -0500

    Cambios en las Entidades

--- WebApplication/Data/Persistence/Entities/User.cs
+++ WebApplication/Data/Persistence/Entities/User.cs
@@ -1,7 +1,8 @@
 ﻿namespace SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Data.Persistence.Entities
 {
-    public class User : Microsoft.AspNetCore.Identity.IdentityUser
+    public class User : Microsoft.AspNetCore.Identity.IdentityUser, SmartSolucionesCuba.SAPRESSC.Core.Persistence.Entities.IEntity<string>
     {
+        public string FullName { get; set; }        
         public Account Account { get; set; }
     }
 }
