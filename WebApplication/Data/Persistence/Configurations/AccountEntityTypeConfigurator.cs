114e54b0f09377b31982735ac28ce675423f0bb0
Teudis Naranjo Ortiz <teudis2008@gmail.com>
2019-01-10 09:07:53 -0500

    Actualizacion de la relacion User-Account
    Signed-off-by: Teudis Naranjo Ortiz <teudis2008@gmail.com>

--- WebApplication/Data/Persistence/Configurations/AccountEntityTypeConfigurator.cs
+++ WebApplication/Data/Persistence/Configurations/AccountEntityTypeConfigurator.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Data.Persistence.Configurations
+﻿namespace SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Data.Persistence.Configurations
 {
-    public class AccountEntityTypeConfigurator: AbstractEntityTypeConfigurator<Entities.Account> 
+    public class AccountEntityTypeConfigurator: SmartSolucionesCuba.SAPRESSC.Core.Persistence.Configurations.AbstractEntityTypeConfigurator<Entities.Account> 
     {
-        protected override string GetTableName() => "accounts";
+        protected override void ApplayRules(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Entities.Account> entityTypeBuilder)
+        {
+            base.ApplayRules(entityTypeBuilder);
+
+            entityTypeBuilder.HasMany(entity => entity.Members).WithOne(entity => entity.Account);
+
+            entityTypeBuilder.HasOne(entity => entity.Representative);
+        }
     }
 }
