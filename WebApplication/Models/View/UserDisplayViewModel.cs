5f51006410462e1dc0c98e358e28ed66bb8acb86
Teudis Naranjo Ortiz <teudis2008@gmail.com>
2019-01-10 09:23:07 -0500

    Adicionado ModelViews para User
    Signed-off-by: Teudis Naranjo Ortiz <teudis2008@gmail.com>

---
+++ WebApplication/Models/View/UserDisplayViewModel.cs
@@ -0,0 +1,32 @@
+﻿using SmartSolucionesCuba.SAPRESSC.Core.Web.Management.Models.View;
+using SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Data.Persistence.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Models.View
+{
+    public class UserDisplayViewModel : IEntityDisplayViewModel<User, string>
+    {
+        public string Id { get ; set ; }
+
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Telefono")]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "Nombre Completo")]
+        public string FullName { get; set; }
+
+        public void Import(User entity)
+        {
+            Id = entity.Id;
+            Email = entity.Email;            
+            PhoneNumber = entity.PhoneNumber;
+            FullName = entity.FullName;
+        }
+    }
+}
---
+++ WebApplication/Models/View/UserWithPasswordInputViewModel.cs
@@ -0,0 +1,37 @@
+﻿using SmartSolucionesCuba.SAPRESSC.Core.Web.Management.Models.View;
+using SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Data.Persistence.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Models.View
+{
+    public class UserWithPasswordInputViewModel : UserInputViewModel   {
+        
+        
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public override User Export()
+        {
+            var model = base.Export();
+            model.PasswordHash = Password;
+            //model.PasswordHash = HashPassword(Password);
+            return model;
+        }
+
+      
+
+    }
+}
