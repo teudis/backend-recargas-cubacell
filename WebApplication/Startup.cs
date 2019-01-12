8f58660672397538c67a70e815cf475f8bfe96c8
Miguel <matorres@smartsolucionescuba.com>
2019-01-12 15:23:01 -0500

    Aplicando algunas pautas en el código y organizando los using (utilizando Click Derecho -> Eliminar y ordenar instrucciones Using o Crl+R, Crl+G), para que sirvan de muestra de lo que se espera ne futuros cambios.

--- WebApplication/Startup.cs
+++ WebApplication/Startup.cs
@@ -1,21 +1,21 @@
 ﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Areas.Identity.Services;
-using Microsoft.AspNetCore.Identity.UI.Services;
-using Microsoft.AspNetCore.DataProtection;
 using Microsoft.Extensions.Logging;
-using SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Data.Persistence.Repositories;
 using SmartSolucionesCuba.SAPRESSC.Core.Persistence.Repositories;
-using SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Data.Persistence.Entities;
+using SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Areas.Identity.Services;
 using SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Data;
-using System.Threading.Tasks;
+using SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Data.Persistence.Entities;
+using SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Data.Persistence.Repositories;
 using System;
+using System.Threading.Tasks;
 
 namespace SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication
 {
@@ -38,15 +38,14 @@ namespace SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddIdentity<User, IdentityRole>(config => {
+                // TODO Remove if not required!!
                 //config.SignIn.RequireConfirmedEmail = true;
-
-            }).AddEntityFrameworkStores<ApplicationDbContext>()
-               .AddDefaultTokenProviders();
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
           
             var defaultCulture = new System.Globalization.CultureInfo("es");
 
@@ -81,16 +80,13 @@ namespace SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication
 
             services.AddSingleton<Microsoft.AspNetCore.Mvc.ViewFeatures.ITempDataProvider, Microsoft.AspNetCore.Mvc.ViewFeatures.CookieTempDataProvider>();
 
-
             services.ConfigureApplicationCookie(options =>
-            {
-                
+            {                
                 options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
                 options.LoginPath = "/Identity/Account/Login";
                 options.AccessDeniedPath = "/Identity/Account/AccessDenied";
                 options.SlidingExpiration = true;
             });
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -123,8 +119,8 @@ namespace SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
-                  name: "areas",
-                  template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                    name: "areas",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
                 );
             });
 
@@ -135,13 +131,18 @@ namespace SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
 
-            CreateRoles(serviceProvider).Wait();
+            if (env.IsDevelopment())
+            {
+                Task.Run(async () =>
+                {
+                    await CreateRoles(serviceProvider);
+                });
+            }
         }
 
 
         private async Task CreateRoles(IServiceProvider serviceProvider)
         {
-
             //adding custom roles
 
             var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
@@ -152,50 +153,34 @@ namespace SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication
 
             IdentityResult roleResult;
 
+            //creating the roles and seeding them to the database
             foreach (var roleName in roleNames)
-
             {
-
-                //creating the roles and seeding them to the database
-
                 var roleExist = await RoleManager.RoleExistsAsync(roleName);
 
                 if (!roleExist)
-
                 {
-
                     roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
-
                 }
-
             }
 
             //creating a super user who could maintain the web app
-
             var poweruser = new User
-
             {
-
                 UserName = Configuration.GetSection("UserSettings")["UserEmail"],
-
                 Email = Configuration.GetSection("UserSettings")["UserEmail"]
-
             };
 
-            string UserPassword = Configuration.GetSection("UserSettings")["UserPassword"];
+            var userPassword = Configuration.GetSection("UserSettings")["UserPassword"];
 
             var _user = await UserManager.FindByEmailAsync(Configuration.GetSection("UserSettings")["UserEmail"]);
 
             if (_user == null)
-
             {
-
-                var createPowerUser = await UserManager.CreateAsync(poweruser, UserPassword);
+                var createPowerUser = await UserManager.CreateAsync(poweruser, userPassword);
 
                 if (createPowerUser.Succeeded)
-
                 {
-
                     //here we tie the new user to the "Admin" role 
                     await UserManager.AddToRoleAsync(poweruser, "Administrator");
                 }
