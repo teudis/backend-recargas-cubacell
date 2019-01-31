f3b829d72aca1b749c9b7476cb05ff1487cf1478
Teudis Naranjo Ortiz <teudis2008@gmail.com>
2019-01-31 09:46:51 -0500

    Agregado el IdRole para la gestion de Roles
    Signed-off-by: Teudis Naranjo Ortiz <teudis2008@gmail.com>

--- WebApplication/Data/Persistence/Entities/User.cs
+++ WebApplication/Data/Persistence/Entities/User.cs
@@ -1,8 +1,12 @@
-﻿namespace SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Data.Persistence.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Data.Persistence.Entities
 {
     public class User : Microsoft.AspNetCore.Identity.IdentityUser, SmartSolucionesCuba.SAPRESSC.Core.Persistence.Entities.IEntity<string>
     {
-        public string FullName { get; set; }        
+        [Required]
+        public string FullName { get; set; }
+        public string IdRole { get; set; }
         public Account Account { get; set; }
     }
 }
