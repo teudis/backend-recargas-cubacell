49e6c54b8a0653b1bcacb5357793d098ac326097
Teudis Naranjo Ortiz <teudis2008@gmail.com>
2019-01-31 09:48:15 -0500

    Actualizado  para le gestion de Roles
    Signed-off-by: Teudis Naranjo Ortiz <teudis2008@gmail.com>

--- WebApplication/Managers/UserProfileManager.cs
+++ WebApplication/Managers/UserProfileManager.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Data.Persistence.Entities;
+using SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Models.View;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,17 +13,19 @@ namespace SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Managers
     {
         private UserManager<User> _userManager;
         private IPasswordHasher<User> _identityPasswordHasher = new PasswordHasher<User>();
+        private RoleManager<IdentityRole> roleManager;
 
-        public UserProfileManager(UserManager<User> _userManager)
+        public UserProfileManager(UserManager<User> _userManager, RoleManager<IdentityRole> roleManager)
         {
             this._userManager = _userManager;
-
+            this.roleManager = roleManager;
+            
         }
 
         public void AddRoleDefault(User User)
         {
-             _userManager.AddToRoleAsync(User, "Manager").GetAwaiter().GetResult();
-            _userManager.AddToRoleAsync(User, "Member").GetAwaiter().GetResult();
+             var rol = roleManager.FindByIdAsync(User.IdRole).GetAwaiter().GetResult().Name;            
+            _userManager.AddToRoleAsync(User, rol).GetAwaiter().GetResult();          
 
         }
       
@@ -31,11 +35,28 @@ namespace SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Managers
              _userManager.UpdateSecurityStampAsync(User).GetAwaiter().GetResult();  
         }
 
+        public List<IdentityRole> GetRoles()
+        {
+           return roleManager.Roles.ToList();          
+           
+        }
+
         public string HashPassword(User user, string password)
         {
             return _identityPasswordHasher.HashPassword(user, password);
         }
-      
+
+        public void UpdateRole(User user)
+        {
+
+            string roldata = _userManager.GetRolesAsync(user).GetAwaiter().GetResult().FirstOrDefault();
+            var rolnew = roleManager.FindByIdAsync(user.IdRole).GetAwaiter().GetResult();
+            if (roldata != rolnew.Name)
+            {
+                _userManager.RemoveFromRoleAsync(user, roldata).GetAwaiter().GetResult();
+                _userManager.AddToRoleAsync(user, rolnew.Name).GetAwaiter().GetResult();
+            }
+        }
     }
 
 
@@ -44,6 +65,8 @@ namespace SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Managers
         void GenerateSecurtyStampAsync(User User);
         void AddRoleDefault(User User);
         string HashPassword(User user, string password);
+        List<IdentityRole> GetRoles();
+        void UpdateRole(User user);
 
     }
 
