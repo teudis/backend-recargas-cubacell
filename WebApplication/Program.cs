07f154499bc2f619f1fab78053769fe2d90ab650
Teudis Naranjo Ortiz <teudis2008@gmail.com>
2019-01-31 09:50:15 -0500

    Actualizado
    Signed-off-by: Teudis Naranjo Ortiz <teudis2008@gmail.com>

--- WebApplication/Program.cs
+++ WebApplication/Program.cs
@@ -10,8 +10,6 @@ namespace SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) => WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
     }
 }
