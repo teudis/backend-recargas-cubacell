51f8a589e0707c6892d7ed2a39557ebc72f522b9
Teudis Naranjo Ortiz <teudis2008@gmail.com>
2019-02-05 12:07:13 -0500

    Nuevos repositorios
    Signed-off-by: Teudis Naranjo Ortiz <teudis2008@gmail.com>

---
+++ WebApplication/Data/Persistence/Repositories/NautaBalanceTuneUpProfileRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartSolucionesCuba.SAPRESSC.Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Data.Persistence.Repositories
+{
+    public class NautaBalanceTuneUpProfileRepository : AbstractEntityRepository<Entities.NautaBalanceTuneUpProfile, int>
+    {
+        public NautaBalanceTuneUpProfileRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
