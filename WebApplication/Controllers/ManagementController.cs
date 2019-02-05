9350225490ed244c570873e45a35b047543e7980
Teudis Naranjo Ortiz <teudis2008@gmail.com>
2019-02-05 12:04:02 -0500

    Controladora  princicpal para acceder a zona de administracion
    Signed-off-by: Teudis Naranjo Ortiz <teudis2008@gmail.com>

---
+++ WebApplication/Controllers/ManagementController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+using SmartSolucionesCuba.SAPRESSC.Core.Web.Common.Controllers;
+using SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Managers;
+
+namespace WebApplication.Controllers
+{
+    [Authorize(Roles = ManagementRoleCodes.ADMINISTRADOR)]
+    public class ManagementController : BaseWebController
+    {
+        public ManagementController(IStringLocalizer<ManagementController> localizer, ILogger<BaseWebController> logger) : base(localizer, logger)
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
