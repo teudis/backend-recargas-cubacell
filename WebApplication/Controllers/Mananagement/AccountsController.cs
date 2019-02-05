339c9871be61053a8a169ee331d3bd63242fac42
Teudis Naranjo Ortiz <teudis2008@gmail.com>
2019-02-05 12:04:55 -0500

    Controladora AccountsController.cs para la administracion de cuentas
    Signed-off-by: Teudis Naranjo Ortiz <teudis2008@gmail.com>

---
+++ WebApplication/Controllers/Mananagement/AccountsController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+using SmartSolucionesCuba.SAPRESSC.Core.Persistence.Repositories;
+using SmartSolucionesCuba.SAPRESSC.Core.Web.Management.Controllers;
+using SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Data.Persistence.Entities;
+using SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Managers;
+using SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Models.View;
+
+namespace WebApplication.Controllers
+{
+    [Area("main")]
+    [Authorize(Roles = ManagementRoleCodes.ADMINISTRADOR)]
+    public class AccountsController : AbstractEntityManagementController<Account, System.Guid, AccountInputViewModel,AccountDisplayViewModel>
+    {
+        private readonly IEntityRepository<User, string> usersRepository;
+        public AccountsController(IEntityRepository<User, string> usersRepository,IEntityRepository<Account, System.Guid> repository, IStringLocalizer<AccountsController> localizer, ILogger<AccountsController> logger) : base(repository, localizer, logger)
+        {
+            this.usersRepository = usersRepository;
+        }
+
+        public override IActionResult Index(int sheet = 1, int limit = 25)
+        {
+            return base.Index(sheet, limit);
+        }
+
+        public override IActionResult Create()
+        {
+            return base.Create();
+        }
+
+        public override IActionResult Create(AccountInputViewModel modelInput)
+        {
+            return base.Create(modelInput);
+        }
+
+        protected override void PreCreate(Account entity, AccountInputViewModel modelInput)
+        {
+            base.PreCreate(entity, modelInput);
+            entity.Representative = usersRepository.Get(modelInput.RepresentativeId);
+
+        }
+
+        public override IActionResult Edit(System.Guid key)
+        {
+            return base.Edit(key);
+        }
+
+        public override IActionResult Edit(AccountInputViewModel modelInput)
+        {
+           return base.Edit(modelInput);
+        }
+
+
+        protected override void PreEdit(Account entity, AccountInputViewModel modelInput)
+        {
+            base.PreEdit(entity, modelInput);
+            entity.Representative = usersRepository.Get(modelInput.RepresentativeId);
+        }
+
+        protected override void PostEdit(Account entity, AccountInputViewModel modelInput)
+        {
+            base.PostEdit(entity, modelInput);           
+
+        }
+
+        public override IActionResult Delete(System.Guid key)
+        {
+            return base.Delete(key);
+        }
+
+        
+        public override IActionResult Delete(AccountDisplayViewModel displayModel)
+        {
+            return base.Delete(displayModel);
+        }
+
+        protected override void PostDelete(Account entity)
+        {
+            base.PostDelete(entity);
+        }
+
+        private void PopulateModelInputForAvailableUser(AccountInputViewModel inputModel)
+        {
+            var entities = usersRepository.GetAll();
+
+            var selectListItems = new List<SelectListItem>();
+
+            foreach (var entity in entities)
+            {
+                selectListItems.Add(new SelectListItem { Value = entity.Id.ToString(), Text = entity.FullName });
+            }
+
+            inputModel.Users = selectListItems;
+        }
+
+        protected override AccountInputViewModel ConfigureCreate(AccountInputViewModel modelInput)
+        {
+            PopulateModelInputForAvailableUser(modelInput);
+            return base.ConfigureCreate(modelInput);
+        }
+
+
+        protected override AccountInputViewModel ConfigurePostFailCreateValidation(AccountInputViewModel modelInput)
+        {
+            PopulateModelInputForAvailableUser(modelInput);
+            return base.ConfigurePostFailCreateValidation(modelInput);
+        }
+
+        protected override AccountInputViewModel ConfigureEdit(Account entity, AccountInputViewModel modelInput)
+        {
+            PopulateModelInputForAvailableUser(modelInput);
+            return base.ConfigureEdit(entity, modelInput);
+        }
+
+        protected override AccountInputViewModel ConfigurePostFailEditValidation(AccountInputViewModel modelInput)
+        {
+            PopulateModelInputForAvailableUser(modelInput);
+            return base.ConfigurePostFailEditValidation(modelInput);
+        }
+
+    }
+}
