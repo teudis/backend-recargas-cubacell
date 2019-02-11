709485dd37777407af347a02dab0853597d57753
Teudis Naranjo Ortiz <teudis2008@gmail.com>
2019-02-11 16:09:33 -0500

    Controladora para la gestion de recargas
    Signed-off-by: Teudis Naranjo Ortiz <teudis2008@gmail.com>

---
+++ WebApplication/Areas/Account/Controllers/RecargaController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Data;
+
+namespace WebApplication.Areas.Account.Controllers
+{
+    [Area("Account")]
+    public class RecargaController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RecargaController(ApplicationDbContext _context)
+        {
+            this._context = _context;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetPerfilNauta()
+        {
+            var result = _context.NautaBalanceTuneUpProfiles.ToList();
+            return Json(result);
+        }
+
+        public IActionResult GetPerfilCubacel()
+        {
+            var result = _context.CellularBalanceTuneUpProfiles.ToList();
+            return Json(result);
+        }
+
+    }
+}
