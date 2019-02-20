f223d163281295c2774c09256e724a6de7a628c8
Teudis Naranjo Ortiz <teudis2008@gmail.com>
2019-02-20 16:17:59 -0500

    Validado el formulario segun el tipo de recarga si es un celular o una telefono de una casa.

--- WebApplication/wwwroot/js/dynamic_form.js
+++ WebApplication/wwwroot/js/dynamic_form.js
@@ -2,17 +2,85 @@
 
             var rowcubacel = 0;
             var rownauta = 0;
+            var datos_validos = 0;
             var select_perfil_nauta = $("<select class='form-control' id='perfil_nauta'></select>");
             var select_perfil_cubacel = $("<select class='form-control' id='perfil_cubacel'></select>");
-            $("#button_send").hide();
+           $("#button_send").hide();
+
+
+   
+     //Enviar Datos
+     $(document).on('click', '#button_send', function (e) {
+
+
+         for (var i = 1; i <= rownauta; i++) {
+
+             var number = $("#nauta" + i).val();             
+             if (!ValidarNauta(number)) {
+
+                 $("#nauta" + i).attr('style', "border-radius: 5px; border:#FF0000 1px solid;");
+                 
+             }
+             else {
+
+                 $("#nauta" + i).attr('style', "border-radius: 5px; border: #00B33C 1px solid;");
+                 datos_validos++;
+             }
+         }
+
+
+         for (var i = 1; i <= rowcubacel; i++) {
+
+             var number = $("#cubacel" + i).val();
+             if (!ValidarCubacel(number)) {
+
+                 $("#cubacel" + i).attr('style', "border-radius: 5px; border:#FF0000 1px solid;");
+
+             }
+             else {
+
+                 $("#cubacel" + i).attr('style', "border-radius: 5px; border: #00B33C 1px solid;");
+                 datos_validos++;
+             }
+         }
+
+         if (datos_validos == ( rowcubacel + rownauta)) {
+
+             console.log("Procesar a Formulario con datos correctos")
+         }
+     });
+
+
+        
+             function ValidarNauta( number ) {
+
+                 var expReg = new RegExp(/^([1-9]{1})+[0-9]{7}$/)
+                 if (expReg.test(number)) {
+
+                     return true;
+                 }
+                 else
+                     return false;
+            }
+
+            function ValidarCubacel(number) {
+
+                 var expReg = new RegExp(/^([52-58])+[0-9]{6}$/)
+                 if (expReg.test(number)) {
+
+                     return true;
+                 }
+                 else
+                     return false;
+             }
 
             //Generar campos cubacel
             $(document).on('click', '#id_cubalcel', function (e) {
                 e.preventDefault();
                 $("#button_send").show();
                 rowcubacel++;
-                var cubacel = '<div id="rowNum' + rowcubacel + '"><h3>Recarga Cubacel</h3><label for="numero_cell" class="col-sm-2 control-label">N&uacute;mero Movil</label><div class="col-sm-4"><input type="text" class="form-control" id="cubacel' + rowcubacel + '"></div><label class="col-sm-3 control-label">Perfil Cubacel</label><div class="col-sm-3" id="select_cubacel' + rowcubacel + '"></div></div><br>';
-                jQuery('#dynamic_inputs').append(cubacel);
+                var cubacel = '<div id="rowNumCubacel' + rowcubacel + '"><h3>Recarga Cubacel</h3><label for="numero_cell" class="col-sm-2 control-label">N&uacute;mero Movil</label><div class="col-sm-4"><input type="text" class="form-control" id="cubacel' + rowcubacel + '"></div><label class="col-sm-3 control-label">Perfil Cubacel</label><div class="col-sm-3" id="select_cubacel' + rowcubacel + '"></div></div><br>';
+                $('#zone_cell').append(cubacel);
                 var copy = select_perfil_cubacel.clone();
                 $("#select_cubacel" + rowcubacel).append(copy);  
             });
@@ -22,8 +90,8 @@
                 e.preventDefault();
                 $("#button_send").show();
                 rownauta++;
-                var nauta = '<div id="rowNum' + rownauta + '"><h3>Recarga Nauta</h3><label for="numero_cell" class="col-sm-2 control-label">N&uacute;mero Casa</label><div class="col-sm-4"><input type="text" class="form-control" id="nauta' + rownauta + '"></div><label class="col-sm-3 control-label">Perfil Nauta</label><div class="col-sm-3" id="select_nauta' + rownauta + '"></div></div><br>';
-                jQuery('#dynamic_inputs').append(nauta); 
+                var nauta = '<div id="rowNumNauta' + rownauta + '"><h3>Recarga Nauta</h3><label for="numero_cell" class="col-sm-2 control-label">N&uacute;mero Casa</label><div class="col-sm-4"><input type="text" class="form-control" id="nauta' + rownauta + '"></div><label class="col-sm-3 control-label">Perfil Nauta</label><div class="col-sm-3" id="select_nauta' + rownauta + '"></div></div><br>';
+                $('#zone_nauta').append(nauta);                 
                 var copy = select_perfil_nauta.clone();
                 $("#select_nauta" + rownauta).append(copy);               
 
