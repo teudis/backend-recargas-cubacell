994f5e84c2e7725c6c6686131dbf383aa6d24eaf
Teudis Naranjo Ortiz <teudis2008@gmail.com>
2019-02-23 11:56:14 -0500

    ModelView para procesar los datos de las recargas Nauta

---
+++ WebApplication/Models/View/RecargaNautaModelView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Models.View
+{
+    public class RecargaNautaModelView
+    {
+       [ Required, EmailAddress]
+        public string EmailAddressTarget { get; set; }
+
+        public int Id{ get; set; }
+    }
+}
