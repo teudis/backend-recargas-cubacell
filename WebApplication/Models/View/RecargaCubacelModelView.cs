bc4f2ada0cb2e6bf0b4fa0cde42fc1f3fff09ddf
Teudis Naranjo Ortiz <teudis2008@gmail.com>
2019-02-23 11:56:02 -0500

    ModelView para procesar los datos de las recargas Cubacel
    Signed-off-by: Teudis Naranjo Ortiz <teudis2008@gmail.com>

---
+++ WebApplication/Models/View/RecargaCubacelModelView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Models.View
+{
+    public class RecargaCubacelModelView
+    {
+
+        [Required]
+        public string PhoneNumberTarget { get; set; }
+
+        public int Id { get; set; }
+    }
+}
