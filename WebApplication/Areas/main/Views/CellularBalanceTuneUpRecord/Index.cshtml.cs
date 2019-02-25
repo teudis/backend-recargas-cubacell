ba498335073e556d53bdf9a7c5aac88c3d0957cf
Teudis Naranjo Ortiz <teudis2008@gmail.com>
2019-02-25 16:55:16 -0500

    Nuevas Vistas para CellularBalanceTuneUpRecordController.cs
    Signed-off-by: Teudis Naranjo Ortiz <teudis2008@gmail.com>

---
+++ WebApplication/Areas/main/Views/CellularBalanceTuneUpRecord/Index.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Areas.Main.Views.CellularBalanceTuneUpRecord
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
