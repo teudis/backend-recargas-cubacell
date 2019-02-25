bc93d2de25b7c3ca2cf09db21b25b2521a7354b9
Teudis Naranjo Ortiz <teudis2008@gmail.com>
2019-02-25 16:56:39 -0500

    Agregado repositorio CellularBalanceTuneUpRecordRepository.cs
    Signed-off-by: Teudis Naranjo Ortiz <teudis2008@gmail.com>

---
+++ WebApplication/Data/Persistence/Repositories/CellularBalanceTuneUpRecordRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartSolucionesCuba.SAPRESSC.Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Data.Persistence.Repositories
+{
+    public class CellularBalanceTuneUpRecordRepository : AbstractEntityRepository<Entities.CellularBalanceTuneUpRecord, long>
+    {
+        public CellularBalanceTuneUpRecordRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
