cb74dcd0012d60a2b24c215cbb1f2f5ce651a659
Teudis Naranjo Ortiz <teudis2008@gmail.com>
2019-02-26 07:40:18 -0500

    ModelsViews  para gestionar Recargas Nauta en Back-End
    Signed-off-by: Teudis Naranjo Ortiz <teudis2008@gmail.com>

---
+++ WebApplication/Models/View/NautaBalanceTuneUpRecordInputViewModel.cs
@@ -0,0 +1,47 @@
+﻿using SmartSolucionesCuba.SAPRESSC.Core.Web.Management.Models.View;
+using SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Data.Persistence.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SSC.CustomSolution.CubansConexion.TuneUpResell.WebApplication.Models.View
+{
+    public class NautaBalanceTuneUpRecordInputViewModel : IEntityInputViewModel<NautaBalanceTuneUpRecord, long>
+    {
+        public long Id { get ; set ; }
+       
+        public System.DateTime Created { get; private set; }
+
+        public User Agent { get; set; }
+
+        public float Amount { get; set; }
+
+        [Required, EmailAddress]
+        public string EmailAddressTarget { get; set; }
+
+        public NautaBalanceTuneUpRecord Export()
+        {
+            var entity = new NautaBalanceTuneUpRecord();
+            Mergue(entity);
+            return entity;
+        }
+
+        public void Import(NautaBalanceTuneUpRecord entity)
+        {
+            Id = entity.Id;
+            Created = entity.Created;
+            Agent = entity.Agent;
+            Amount = entity.Amount;
+            EmailAddressTarget = entity.EmailAddressTarget;
+        }
+
+        public void Mergue(NautaBalanceTuneUpRecord entity)
+        {
+            entity.Amount = Amount;
+            entity.Agent = Agent;
+            entity.EmailAddressTarget = EmailAddressTarget;
+        }
+    }
+}
