4bb6c4a173fb437c65da26e56680429e6f4f0582
Teudis Naranjo Ortiz <teudis2008@gmail.com>
2019-03-06 11:40:53 -0500

    Ficheros JavaScript
    Signed-off-by: Teudis Naranjo Ortiz <teudis2008@gmail.com>

---
+++ WebApplication/wwwroot/js/searchaccount.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+
+    $("#emailtarget").hide();
+    $("#phonetarget").hide();
+
+    $('#perfilrecarga').change(function () {
+
+        var selected = $("#perfilrecarga").val();
+        switch (selected) {
+
+            case "nauta": $("#emailtarget").show(); $("#phonetarget").hide(); break;
+            case "cubacel": $("#phonetarget").show(); $("#emailtarget").hide(); break;
+            default: $("#emailtarget").hide(); $("#phonetarget").hide();
+        }
+    });
+
+    $('#FechaInicio').datetimepicker({
+
+        locale: 'es'
+    });
+
+    $('#FechaFin').datetimepicker({
+
+        locale: 'es'
+    });
+
+
+
+});
